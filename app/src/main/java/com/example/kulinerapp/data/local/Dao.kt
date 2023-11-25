8bc5234c121a12782e9fb4ad1e539414155231d8
ajihermansya <ajiheransya14@gmail.com>
2023-11-26 00:27:58 +0700

    yeayy

--- app/src/main/java/com/example/kulinerapp/data/local/Dao.kt
+++ app/src/main/java/com/example/kulinerapp/data/local/Dao.kt
@@ -8,21 +8,23 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface Dao {
-    @Query("SELECT * FROM kuliner")
-    fun getAllKuliner(): Flow<List<KulinerEntity>>
 
-    @Query("SELECT * FROM kuliner WHERE isFavorite = 1")
-    fun getAllFavoriteKuliner(): Flow<List<KulinerEntity>>
-
-    @Query("SELECT * FROM kuliner WHERE id = :id")
-    fun getKuliner(id: Int): Flow<KulinerEntity>
-
-    @Query("SELECT * FROM kuliner WHERE name LIKE '%' || :query || '%'")
-    fun searchKuliner(query: String): Flow<List<KulinerEntity>>
+    @Query("UPDATE kuliner SET isFavorite = :isFavorite WHERE id = :id")
+    suspend fun updateFavoriteKuliner(id: Int, isFavorite: Boolean)
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertAllKuliner(kulinerList: List<KulinerEntity>)
 
-    @Query("UPDATE kuliner SET isFavorite = :isFavorite WHERE id = :id")
-    suspend fun updateFavoriteKuliner(id: Int, isFavorite: Boolean)
+    @Query("SELECT * FROM kuliner WHERE isFavorite = 1")
+    fun getAllFavoriteKuliner(): Flow<List<KulinerEntity>>
+
+    @Query("SELECT * FROM kuliner")
+    fun getAllKuliner(): Flow<List<KulinerEntity>>
+
+    @Query("SELECT * FROM kuliner WHERE name LIKE '%' || :query || '%'")
+    fun searchKuliner(query: String): Flow<List<KulinerEntity>>
+
+
+    @Query("SELECT * FROM kuliner WHERE id = :id")
+    fun getKuliner(id: Int): Flow<KulinerEntity>
 }
