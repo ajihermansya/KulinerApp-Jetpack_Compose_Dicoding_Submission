482bb2ffd43309181b8775a9696392d2d7615714
ajihermansya <ajiheransya14@gmail.com>
2023-11-25 09:45:52 +0700

    add theme-done

---
+++ app/src/main/java/com/example/kulinerapp/home/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.kulinerapp.home
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.kulinerapp.ui.theme.KulinerAppTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            KulinerAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainScreen()
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/kulinerapp/home/MainScreen.kt
@@ -0,0 +1,8 @@
+package com.example.kulinerapp.home
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun MainScreen(){}
